c5b9a84b7b387e455fe8e9194e19f25191fe7ab1
annun <annun@DESKTOP-TUF8GF9>
2022-08-22 12:58:30 +0530

    Add project files.

---
+++ Parameters_Assingnment/Q2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Parameters_Assingnment
+{
+    internal class Q2
+    {
+
+        public static void Main()
+        {
+            string sentence = "DotNet is technology and DotNet is interoperable and DotNet is simple";
+            string[] words = sentence.Split(new char[] { ' ', ',', '.', '!', ':', '?', ';' }, StringSplitOptions.RemoveEmptyEntries); // this splits the string into the words
+            var groups = words.GroupBy(x => x); // this groups the words
+            foreach (var group in groups)
+            {
+                Console.WriteLine("{0} - {1}", group.Key, group.Count());
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Parameters_Assingnment/Q3.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameters_Assingnment
+{
+    internal class Q3
+    {
+        
+        
+            public static int Sum(int num1, int num2)
+            {
+                int total;
+                total = num1 + num2;
+                return total;
+            }
+
+
+            public static void Main()
+        {
+               
+            Console.Write("\n\nFunction to calculate the sum of two numbers :\n");
+                Console.Write("--------------------------------------------------\n");
+                Console.Write("Enter a number: ");
+                int n1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter another number: ");
+                int n2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\nThe sum of two numbers is : {0} \n", Sum(n1, n2));
+                Console.ReadLine(); 
+            }
+        }
+
+    }
